963ab77e22fb8d118b9bbd9239d98774dbbb00ea
tae0 <nadalfedderer@gmail.com>
2024-09-11 17:37:14 +0900

    chore: MenuSampleDTO 클래스 삭제

--- src/main/java/com/pyeonrimium/queuing/menus/domains/dtos/MenuSampleDTO.java
+++
@@ -1,9 +0,0 @@
-package com.pyeonrimium.queuing.menus.domains.dtos;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class MenuSampleDTO {
-
-	List<String> list = new ArrayList<String>();
-}
