574aca06ae77886e04b6d5c32161ed8e9b6e4df1
tae0 <nadalfedderer@gmail.com>
2024-09-11 17:20:53 +0900

    chore: MenuSampleDTO 클래스 추가

---
+++ src/main/java/com/pyeonrimium/queuing/menus/domains/dtos/MenuSampleDTO.java
@@ -0,0 +1,9 @@
+package com.pyeonrimium.queuing.menus.domains.dtos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuSampleDTO {
+
+	List<String> list = new ArrayList<String>();
+}
