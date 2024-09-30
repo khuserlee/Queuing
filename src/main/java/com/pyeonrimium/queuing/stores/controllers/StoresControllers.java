13c2684762eef2472934d29c667118771647fc2b
nigachampion <wlsdud950831@daum.net>
2024-09-30 18:56:29 +0900

    feat: 매장등록 구현중

--- src/main/java/com/pyeonrimium/queuing/stores/controllers/StoresControllers.java
+++ src/main/java/com/pyeonrimium/queuing/stores/controllers/StoresControllers.java
@@ -4,32 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-
 import com.pyeonrimium.queuing.stores.domains.entities.StoreEntity;
 import com.pyeonrimium.queuing.stores.services.StoreService;
 
 @Controller
+
 public class StoresControllers {
 
 	//StoreService 연결
 	@Autowired
 	private StoreService storeService;
 
-	//TODO: 매장정보등록 화면 불러오기
+	
+	// 매장정보등록 화면 불러오기
 	@GetMapping("/stores/form")
 	public String getStoreform() {
 		System.out.println("[storeControllers] getStoreform()");
 		return "/stores/storeRegistration";
 	}
 	
-	// TODO: 매장 정보 저장(Create)
-	@PostMapping("/stores")
-	public String createStore() {
-		System.out.println("[StoresControllers] createStore()");
-		return "";
-	}
+	// TODO: 매장 정보  등록(저장)(Create)
 	
 	
 	// TODO: 매장 정보 조회(Read)
